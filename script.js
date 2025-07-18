37c57b98afa47e65fb8e3328ba5dac7c4723106e
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-18 15:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-
+let yourArray =[
+	[
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ],
+  [
+	t:true;
+	f:false;
+  ];
+[
+	firstname:"asish";
+	age:20;
+],[
+	email:"deb@gmail.com";
+]
+]
